16038e3162f5eeb3141c5877bd055ccdd5ecc72e
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-07-15 18:31:05 +0700

    feat(login-layout): add login layout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,9 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        return view('auth.login2',[
+            'title' => "Login | Aplikasi Real Estate ",
+        ]);
     }
 
     /**
